4de9bbc7edc51df6eb376b8f4255223dd3894abf
Lohith.S Leo <63157889+JarvisAI-Lolu@users.noreply.github.com>
2020-08-20 21:29:36 +0530

    Add files via upload

---
+++ speech.js
@@ -0,0 +1,6 @@
+var msg= new SpeechSynthesisUtterance();
+msg.text="Hello Boss";
+window.speechSynthesis.speak(msg);
+var ms = new SpeechSynthesisUtterance();
+ms.text="I am Jarvis   an A I virual assistant created by Lohith S";
+window.speechSynthesis.speak(msg);
